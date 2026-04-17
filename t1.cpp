3cd9671c1f7bd26c7ac6402ba7460e219a905240
arthur-portella <arthur.portella2005@gmail.com>
2026-04-17 19:30:14 -0300

    fim

--- t1.cpp
+++ t1.cpp
@@ -2,21 +2,29 @@
 using namespace std;
 
 char proximo;
-int simbolo;
+string simbolo;
 int linha = 1;
 ifstream arquivo;
 string atomo;
-
-const int codigo_identificador = 45;
-const int codigo_numero = 40;
 
 map<string, int> palavras_reservadas = {
     {"program", 1}, {"label", 2}, {"var", 3}, {"procedure", 4}, {"function", 5}, {"begin", 6}, {"end", 7}, {"if", 8}, {"then", 9}, {"else", 10}, {"while", 11}, {"do", 12}, {"or", 13}, {"and", 14}, {"div", 15}, {"not", 16}, {"integer", 17}, {"read", 18}, {"write", 19}
 };
 
 map<string, int> simbolos_especiais = {
-    {":=", 20}, {";", 21}, {",", 22}, {":", 23}, {"(", 24}, {")", 25}, {".", 26}, {"+", 27}, {"-", 28}, {"*", 29}, {"=", 30}, {"<", 31}, {">", 32}, {"<=", 33}, {">=", 34}, {"<>", 35}
+    {":=", 1}, {";", 2}, {",", 3}, {":", 4}, {"(", 5}, {")", 6}, {".", 7}, {"+", 8}, {"-", 9}, {"*", 10}, {"=", 11}, {"<", 12}, {">", 13}, {"<=", 14}, {">=", 15}, {"<>", 16}
 };
+
+int contador_id = 1;
+map<string, int> tabela_simbolos = {};
+
+int ENCONTRA_TABELA(string aux) {
+    if (tabela_simbolos.count(aux)) 
+        return tabela_simbolos[aux];
+
+    tabela_simbolos[aux] = contador_id;
+    return contador_id++;
+}
 
 void PROXIMO() {
     if (arquivo.get(proximo)) {
@@ -34,30 +42,44 @@ void ERRO(string msg) {
     exit(1);
 }
 
-int CODIGO(string s) {
+string CODIGO(string s) {
     string aux = s;
     transform(aux.begin(), aux.end(), aux.begin(), ::tolower);
 
-    if (palavras_reservadas.count(aux)) 
-        return palavras_reservadas[aux];
-    
-    if (simbolos_especiais.count(aux)) 
-        return simbolos_especiais[aux];
+    if (palavras_reservadas.count(aux)) {
+        string str = to_string(palavras_reservadas[aux]);
+        return "p_res[" + str + "]";
+    }
+
+    if (simbolos_especiais.count(aux)) {
+        string str = to_string(simbolos_especiais[aux]);
+        return "s_esp[" + str + "]";
+    }
         
-    if (isalpha(aux[0]))
-        return codigo_identificador;
+    if (isalpha(aux[0])) {
+        int codigo_numerico = ENCONTRA_TABELA(aux);
+        string str = to_string(codigo_numerico);
+        return "id[" + str + "]";
+    }
 
     if (isdigit(aux[0]))
-        return codigo_numero;
+        return "num[" + aux + "]";
 
-    return -1;
+    return "ERRO";
 }
 
 void ANALISADOR_LEXICO() {
     atomo = "";
 
-    while (proximo != EOF && isspace(proximo)) 
+    while (proximo != EOF && isspace(proximo)) {
+        if (proximo == '\n') {
+            cout << '\n';
+            cout << linha << "\t";
+        }
+        else
+            cout << proximo;
         PROXIMO();
+    }
 
     if (proximo == EOF)
         return;
@@ -75,7 +97,7 @@ void ANALISADOR_LEXICO() {
         
         simbolo = CODIGO(s);
 
-        if (simbolo != -1)
+        if (simbolo != "ERRO")
             cout << "<" << s << ", " << simbolo << "> ";
         else
             ERRO("Simbolo especial nao reconhecido: " + s);
@@ -93,14 +115,14 @@ void ANALISADOR_LEXICO() {
 
     else if (isdigit(proximo)) {
         do {
-            atomo +=proximo;
+            atomo += proximo;
             PROXIMO();
         } while (isdigit(proximo));
 
         if (isalpha(proximo))
             ERRO("Identificador ou numero nao reconhecidos");
 
-        simbolo = codigo_numero;
+        simbolo = CODIGO(atomo);
         cout << "<" << atomo << ", " << simbolo << "> ";
     }
 
@@ -115,6 +137,8 @@ int main() {
     if (!arquivo.is_open()) {
         cout << "Arquivo Trab1_Compiladores.txt nao foi aberto!" << '\n';
     }
+
+    cout << "1\t";
 
     PROXIMO();
     while(proximo != EOF) 
